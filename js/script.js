2331766595c167ef8703ecc65243db6453927415
vivek chhabra <vivekcb8642@gmail.com>
2023-08-06 19:10:17 +0530

    add skills section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // element variables
 const header = document.querySelector(".header");
 const links = document.querySelectorAll(".header nav a");
+const growth = document.querySelectorAll('.skill-box .progress .growth')
+const progressBars = document.querySelectorAll('.skill-box .progress .bar span')
 
 // adjusting the height of the navbar
 window.addEventListener("scroll", () => {
@@ -19,4 +21,11 @@ for (let i = 0; i < links.length; i++) {
         links[i].classList.add("active");
         pos = i;
     });
+}
+
+console.log(growth)
+
+// showing growth bar
+for(let i = 0; i < growth.length; i++) {
+    progressBars[i].style = `width: ${growth[i].innerHTML}`
 }
